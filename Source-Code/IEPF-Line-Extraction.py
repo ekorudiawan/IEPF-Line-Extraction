43cbaeb63e162e1b70f68858c90dbcc68b1f37e4
Eko Rudiawan Jamzuri <ekorudiawan@gmail.com>
2018-03-22 21:27:58 +0700

    Update with function

--- Source-Code/IEPF-Line-Extraction.py
+++ Source-Code/IEPF-Line-Extraction.py
@@ -3,204 +3,198 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
+def measPointToPoint(varP1, varP2):
+    return math.sqrt(((varP2[0]-varP1[0])*(varP2[0]-varP1[0])) + ((varP2[1]-varP1[1])*(varP2[1]-varP1[1])))
+
+# https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+def measPointToLine(varPk, varPl, varP0):
+    return abs((varPl[1]-varPk[1])*varP0[0] - (varPl[0]-varPk[0])*varP0[1] + varPl[0]*varPk[1] - varPl[1]*varPk[0]) / math.sqrt( math.pow((varPl[0] - varPk[0]),2) + math.pow((varPl[1] - varPk[1]),2) )
+
 # Fungsi IEPF
 # Input berupa list koordinat titik dan list endpoint dari titik 
+# Ouput berupa list persamaan garis dalam bentuk Ax + By + C = 0
 def iepfFunction(dThreshold, ptList, ePtList):
     maxDPtToLine = 0
-    # dThreshold = 60
-    ptIndex = -1
-    k = 0
-    # ePtList[0].append(80)
-    # ePtList[1].append(67)
-    # print 'awal '       
-    # print ePtList
+    breakPointIndex = -1
 
-    jumlahTitik = len(ptList[0])
+    # listLineFunc = [[0,0]]
+    # del listLineFunc[:]
+    # listLineFunc.append([])
+    # listLineFunc.append([])
+    # listLineFunc.append([])
+
+    # listLineEndPoint = [[0,0]]
+    # del listLineEndPoint[:]
+    # listLineEndPoint.append([])
+    # listLineEndPoint.append([])
+    # listLineEndPoint.append([])
+
+    # jumlahTitik = len(ptList[0])
     jumlahEndpoint = len(ePtList[0])
-    
-    # print 'Jumlah Endpoint Awal{}'.format(jumlahEndpoint)
 
+    # loop sebanyak jumlah end point yang diinputkan
     for i in range(0, jumlahEndpoint-1):
+        # A = y2 - y1 / x2 - x1
         varA = float(ePtList[1][i+1] - ePtList[1][i]) / float(ePtList[0][i+1] - ePtList[0][i])
-        # listPersamaanGaris[0].append(varA)
+        # B = -1
         varB = -1.00
-        # C = Y - MX
+        # C = y - Ax
         varC = float(ePtList[1][i] - varA * ePtList[0][i])
-        print 'IEPF Line Function {}x  + {}y + {} = 0'.format(varA, varB, varC)
-        # print 'Jumlah Titik {}'.format(jumlahTitik)
-        # loop sebanyak jumlah titik
+        # print 'IEPF Line Function {}x  + {}y + {} = 0'.format(varA, varB, varC)
+        # listLineFunc[0].append(varA)
+        # listLineFunc[1].append(varB)
+        # listLineFunc[2].append(varC)
+        # loop sebanyak jumlah titik yang berada diantara endpoint
         for j in range(ePtList[2][i],ePtList[2][i+1]):
             # print 'j = {}'.format(j)
             if j == 0 or j == ePtList[2][i]:
                 continue
+            # Pengukuran jarak titik ke line
+            # d = | ax1 + by1 + c / sqrt(a^2 + b^2) |
             dPtToLine =  float(abs((varA*ptList[0][j] + varB*ptList[1][j] + varC) / (math.sqrt(varA*varA + varB*varB))))
             # print 'D = {}'.format(dPtToLine)
             if dPtToLine > dThreshold:
                 if (dPtToLine > maxDPtToLine):
                     maxDPtToLine = dPtToLine
-                    ptIndex = j
-                    
-            # print 'index = {}'.format(ptIndex)
-    
-    # print 'akhir '       
-    
-    # print 'ptindex'
-    # print ptIndex
-    if(ptIndex != -1):
-        ePtList[0].insert(jumlahEndpoint-1, ptList[0][ptIndex])
-        ePtList[1].insert(jumlahEndpoint-1, ptList[1][ptIndex])
-        ePtList[2].insert(jumlahEndpoint-1, ptIndex)
-        print ePtList
-        iepfFunction(dThreshold, ptList, ePtList)
-    else:
-        plt.title("IEPF Algorithm")
-        plt.plot(ptList[0], ptList[1], 'ro')
-        plt.plot(ePtList[0], ePtList[1])
-        plt.axis([0, 1000, 0, 1000])
-        plt.show()
-        print "Calc done"
+                    breakPointIndex = j
 
+    if(breakPointIndex != -1):
+        # Cari nilai MNE        
+        ePtList[0].insert(jumlahEndpoint-1, ptList[0][breakPointIndex])
+        ePtList[1].insert(jumlahEndpoint-1, ptList[1][breakPointIndex])
+        ePtList[2].insert(jumlahEndpoint-1, breakPointIndex)
+
+        # pawal = [ePtList[0][jumlahEndpoint-2],ePtList[1][jumlahEndpoint-2]]
+        # pakhir = [10,10]
+        # pnol = [5,4]
+        # varMNE0 = maxDPtToLine / measPointToLine()
+        # print ePtList
+        ePtList = iepfFunction(dThreshold, ptList, ePtList)
+    # else:
+        # plt.title("IEPF Algorithm")
+        # plt.plot(ptList[0], ptList[1], 'ro')
+        # plt.plot(ePtList[0], ePtList[1])
+        # plt.axis([0, 1000, 0, 1000])
+        # plt.show()
+        # print listLineFunc
+    return ePtList
+
+def mergeLine(mneThreshold, ptList, ePtList):
+    jumlahEndpoint = len(ePtList[0])
+    for i in range(0, jumlahEndpoint-2):
+        # print 'garis'
+        varPk = [ePtList[0][i] , ePtList[1][i]]
+        varPl = [ePtList[0][i+2] , ePtList[1][i+2]]
+        varP0 = [ePtList[0][i+1] , ePtList[1][i+1]]
+        varMaxDistance = measPointToLine(varPk,varPl,varP0)
+        varPk = [ePtList[0][i] , ePtList[1][i]]
+        varPl = [ePtList[0][i+1] , ePtList[1][i+1]]
+        prevIndex = ePtList[2][i+1] - 1
+        nextIndex = ePtList[2][i+1] + 1
+        # print 'PREV {} NEXT{}'.format(prevIndex, nextIndex) 
+        varP0 = [ptList[0][prevIndex], ptList[1][nextIndex]]
+        # print 'K {},{} L {},{} P0 {},{}'.format(varPk[0], varPk[1], varPl[0], varPl[1], varPl[0], varPl[1])
+        print varPk
+        print varPl
+        print varP0
+        xx = measPointToLine(varPk,varPl,varP0)
+        varMNEprev = varMaxDistance / xx
+        # print 'MNE Prev {}'.format(varMNEprev)
+        print 'Prev list'
+        print ePtList
+        if varMNEprev > 2:
+            # remX = ePtList[0][i+1]
+            # remY = ePtList[1][i+1]
+            # remIndex = ePtList[2][i+1]
+            # ePtList[0].remove(remX)
+            ePtList[0].pop(i+1)
+            ePtList[1].pop(i+1)
+            ePtList[2].pop(i+1)
+        # print 'K {},{} L {},{}'.format(varPk[0], varPk[1], varPl[0], varPl[1],)
+        # for j in range(ePtList[2][i],ePtList[2][i+1]):
+        print 'last list'
+        print ePtList
+    return ePtList
 
 def main():
-    listTitikP = [[5,5],[200,550],[700,550],[600,150]]
-    # listTitikP = [[5,5],[450,950]]
-    listKoorTitik = [[0,0]]
-    listM = [0,0]
-    listC = [0,0]
+    listLineL = [[5,5],[200,550],[700,550],[600,150]]
+    listPointP = [[0,0]]
+    listGradientL = [0,0]
+    listConstantaL = [0,0]
     # Clear list
-    del listM[:]
-    del listC[:]
-    for i in range (0,len(listTitikP)-1):
-        # m = (y2 - y1) / (x2 - x1)
-        m = float(listTitikP[i+1][1] - listTitikP[i][1]) / float(listTitikP[i+1][0] - listTitikP[i][0])
-        # print m
-        # c = y - m * x
-        c = float(listTitikP[i][1] - m * listTitikP[i][0])
-        # print c
-        listM.append(m)
-        listC.append(c)
-        # print(listTitikP[1][1])
+    del listGradientL[:]
+    del listConstantaL[:]
+    for i in range (0,len(listLineL)-1):
+        # M = (y2 - y1) / (x2 - x1)
+        varM = float(listLineL[i+1][1] - listLineL[i][1]) / float(listLineL[i+1][0] - listLineL[i][0])
+        # C = y - m * x
+        varC = float(listLineL[i][1] - varM * listLineL[i][0])
+        listGradientL.append(varM)
+        listConstantaL.append(varC)
 
-    del listKoorTitik[:]
-    listKoorTitik.append([])
-    listKoorTitik.append([])
-    # print listKoorTitik
-    # Plot titik 
-    # print (len(listTitikP))
-    jumlahTitik = 0
-    for i in range(0,len(listTitikP)-1):
+    del listPointP[:]
+    listPointP.append([])
+    listPointP.append([])
+
+    for i in range(0,len(listLineL)-1):
         step = 0
-        if(listTitikP[i][0] > listTitikP[i+1][0]):
+        if(listLineL[i][0] > listLineL[i+1][0]):
             step = -10
-        elif(listTitikP[i][0] < listTitikP[i+1][0]):
+        elif(listLineL[i][0] < listLineL[i+1][0]):
             step = 10
-        for j in range(listTitikP[i][0],listTitikP[i+1][0],step):
-            # Random gaussian untuk simulasi noise
-            mu, sigma = 0 , 0.1
-            k = random.randint(-10, 10)
-            # print k
-            # l = np.random.normal(mu, sigma, k)
-            x = float(j + k)
-            # x = float(j)
+        for j in range(listLineL[i][0],listLineL[i+1][0],step):
+            # Random noise
+            # simNoise = 0
+            simNoise = random.randint(-20, 20)
+            pointX = float(j + simNoise)
             # y = mx + c
-            y = float((listM[i] * x + listC[i]) + k )
-            # print x
-            # print y
-            # print i
-            listKoorTitik[0].append(x)
-            listKoorTitik[1].append(y)
-            jumlahTitik += 1
+            pointY = float((listGradientL[i] * pointX + listConstantaL[i]) + simNoise )
+            listPointP[0].append(pointX)
+            listPointP[1].append(pointY)
 
-    # print listKoorTitik
-    distance = 0
-    lastDistance = 0
-    maxDistance = 0
-    p0 = 0
-    pN = jumlahTitik-1
+    # Simulasi endpoint
+    '''
+    del listPointP[:]
+    listPointP.append([])
+    listPointP.append([])
 
-    # print 'Maximum D P{}-P{} = {}'.format(p0, pN, maxDistance)
-    # print 'P0 {},{} - Pn {},{}'.format(listKoorTitik[0][p0], listKoorTitik[1][p0], listKoorTitik[0][pN], listKoorTitik[1][pN])
-    # Kalkulasi persamaan garis dari 2 titik terjauh
-    listPersamaanGaris = [[0,0,0]] # Ax + Bx + C = 0
-    del listPersamaanGaris[:]
-    listPersamaanGaris.append([])
-    varA = float(listKoorTitik[1][pN] - listKoorTitik[1][p0]) / float(listKoorTitik[0][pN] - listKoorTitik[0][p0])
-    listPersamaanGaris[0].append(varA)
-    varB = -1.00
-    # C = Y - MX
-    varC = float(listKoorTitik[1][p0] - varA * listKoorTitik[0][p0])
-    # print 'Gradient P{}-P{} = {}'.format(p0, pN, varA)
-    # print 'Persamaan garis {}x + {}y + {} = 0'.format(varA,varB,varC)
+    listPointP[0] = [0,100,200,300,350,505,450,500,550,600]
+    listPointP[1] = [300,350,400,501,450,600,350,300,200,100]
+    '''
 
-    # List predicted line point
-    # Isinya point x,y dari titik endpoint
-    listPredLinePt = [[0,0]]
-    del listPredLinePt[:]
-    listPredLinePt.append([])
-    # Ambil nilai x dan y dari koordinat titik P0
-    
-    listPredLinePt.append([])
-    listPredLinePt[0].append(listKoorTitik[0][p0])
-    listPredLinePt[1].append(listKoorTitik[1][p0])
-    # Ambil nilai x dan y dari koordinat titik PN
-    listPredLinePt[0].append(listKoorTitik[0][pN])
-    listPredLinePt[1].append(listKoorTitik[1][pN])
-    dThreshold =200.00
-    # print listPredLinePt
-    # Hitung jarak antar titik
-    foundBreakpoint = 1
-    maxDPtToLine = 0
-    ptIndex = 0
-    for i in range(0,jumlahTitik):
-        if i == p0 or i == pN:
-            continue
-        # d = | ax1 + by1 + c / sqrt(a^2 + b^2) |
-        dPtToLine =  float(abs((varA*listKoorTitik[0][i] + varB*listKoorTitik[1][i] + varC) / (math.sqrt(varA*varA + varB*varB))))
-        # print 'Jarak titik ke garis'
-        # print dPtToLine
-        # print foundBreakpoint
-        if dPtToLine > dThreshold:
-            if (dPtToLine > maxDPtToLine):
-                maxDPtToLine = dPtToLine
-                ptIndex = i
-                # foundBreakpoint += 1
-            
-    listPredLinePt[0].insert(foundBreakpoint, listKoorTitik[0][ptIndex])
-    listPredLinePt[1].insert(foundBreakpoint, listKoorTitik[1][ptIndex])
+    # Input 2 buah endpoint untuk masukan awal
+    endPoint0 = 0
+    endPointN = len(listPointP[0])-1
 
-    # INPUT ENDPOINT UNTUK MASUKAN
-    # print listPredLinePt
     listEndPoint = [[0,0]]
     del listEndPoint[:]
     listEndPoint.append([])
     listEndPoint.append([])
     listEndPoint.append([])
-    # x y P0
-    listEndPoint[0].append(listKoorTitik[0][p0])
-    listEndPoint[1].append(listKoorTitik[1][p0])
-    listEndPoint[2].append(p0)
 
-    # simulasi titik tengah
-    # listEndPoint[0].append(listKoorTitik[0][45])
-    # listEndPoint[1].append(listKoorTitik[1][45])
-    # listEndPoint[2].append(45)
+    # endPoint0 x,y coordinat dan index
+    listEndPoint[0].append(listPointP[0][endPoint0])
+    listEndPoint[1].append(listPointP[1][endPoint0])
+    listEndPoint[2].append(endPoint0)
 
-    # xy PN
-    listEndPoint[0].append(listKoorTitik[0][pN])
-    listEndPoint[1].append(listKoorTitik[1][pN])
-    listEndPoint[2].append(pN+1)
+    # endPointN x,y coordinat dan index
+    listEndPoint[0].append(listPointP[0][endPointN])
+    listEndPoint[1].append(listPointP[1][endPointN])
+    listEndPoint[2].append(endPointN+1)
 
-    # listEndPoint[0].append(50)
-    # listEndPoint[1].append(30)
-
-    # listEndPoint[0].append(60)
-    # listEndPoint[1].append(40)
-
-    # print listEndPoint
-    # print listKoorTitik
-    iepfFunction(100, listKoorTitik, listEndPoint)
-
-    
+    # fungsi IEPF dengan threshold 100
+    lsih = iepfFunction(80, listPointP, listEndPoint)
+    print lsih
+    # lsih = mergeLine(9,listPointP,lsih)
+    plt.title("IEPF Algorithm")
+    plt.plot(listPointP[0], listPointP[1], 'ro')
+    plt.plot(lsih[0], lsih[1])
+    plt.axis([0, 1000, 0, 1000])
+    plt.show()
+    # pawal = [0,0]
+    # pakhir = [10,10]
+    # pnol = [5,4]
+    # print measPointToLine(pawal,pakhir,pnol)
 
 if __name__ == "__main__":
     main()
